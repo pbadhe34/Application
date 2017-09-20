b716181d444dc194c5dc27ea60e02772f90b880d
Prakash Badhe <prakash.badhe@agilesoft.in>
2017-09-20 11:27:57 +0530

    Now updated

--- app/src/test/java/server/com/application/ExampleUnitTest.java
+++ app/src/test/java/server/com/application/ExampleUnitTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        System.out.print("This is test");
+        System.out.print("This is test now");
         assertEquals(4, 2 + 5);
     }
 }
